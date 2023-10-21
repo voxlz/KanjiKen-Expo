1c4659c15dc6571bcbe3f092550366ec4ee73b8f
Torben Nordtorp <torben.nordtorp@icloud.com>
2023-10-21 08:08:59 +0200

    Init expo with typescript and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app.d.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
