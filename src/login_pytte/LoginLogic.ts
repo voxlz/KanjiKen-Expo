76a78deafeade62ac94f4a2e2f1de06e6ba96d3c
Torben Nordtorp <torben.nordtorp@icloud.com>
2023-12-17 11:56:38 +0100

    made it work

--- src/login_pytte/LoginLogic.ts
+++ src/login_pytte/LoginLogic.ts
@@ -3,26 +3,39 @@ import auth from '@react-native-firebase/auth'
 import { GoogleSignin } from '@react-native-google-signin/google-signin'
 import { router } from 'expo-router'
 
-GoogleSignin.configure({})
+GoogleSignin.configure({
+    webClientId:
+        '664861145037-kp5gtqeo02bjoha74nqeglg8q5b1vdo7.apps.googleusercontent.com',
+})
 
 export const signInWithGoogle = async () => {
     // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true })
+    const hasPlay = await GoogleSignin.hasPlayServices({
+        showPlayServicesUpdateDialog: true,
+    })
 
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn()
+    if (hasPlay) {
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn()
 
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+        if (idToken) {
+            // Create a Google credential with the token
+            const googleCredential = auth.GoogleAuthProvider.credential(idToken)
 
-    // Sign-in the user with the credential
-    return auth()
-        .signInWithCredential(googleCredential)
-        .then((user) => {
-            console.log('signed in with google', user)
-            router.back()
-        })
-        .catch((err) => console.log('err', err))
+            // Sign-in the user with the credential
+            return auth()
+                .signInWithCredential(googleCredential)
+                .then((user) => {
+                    console.log('signed in with google', user)
+                    router.back()
+                })
+                .catch((err) => console.log('err', err))
+        } else {
+            console.log('no idToken found')
+        }
+    } else {
+        console.log('has no play: Google login wont work.')
+    }
 }
 
 export const signInWithTwitter = async () => {
