849e43500a25c6d9a023abc54841aa2e932785fc
Torben Nordtorp <torben.nordtorp@icloud.com>
2023-12-29 20:04:23 +0100

    removed unused code

--- src/contexts/DragContextProvider.tsx
+++ src/contexts/DragContextProvider.tsx
@@ -1,16 +1,24 @@
-import React, { Dispatch, FC, useReducer, useRef, useState } from 'react'
+import React, {
+    Dispatch,
+    FC,
+    useCallback,
+    useReducer,
+    useRef,
+    useState,
+} from 'react'
 import { dropInfoEqual, dropsReducer } from '../reducers/dropsReducer'
 import { DropInfo, XY } from '../types/dropInfo'
 import { createContext as CC } from '../utils/react'
 
 type DropsDispatchType = Dispatch<Parameters<typeof dropsReducer>[1]>
-type SuccessfulDrop = (glyph: string) => DropInfo | undefined
+// type SuccessfulDrop = (glyph: string) => DropInfo | undefined
 
 // Create contexts
 export const DropsContext = CC<DropInfo[]>()
+export const DropsFindContext = CC<(glyph?: string) => DropInfo | undefined>()
 export const DropsDispatchContext = CC<DropsDispatchType>()
 export const HoverContext = CC<DropInfo>() // What dropLocation am I currently hovering?
-export const WasSuccessfulDropContext = CC<SuccessfulDrop>() // What dropLocation was the element dropped on?
+// export const WasSuccessfulDropContext = CC<SuccessfulDrop>() // What dropLocation was the element dropped on?
 export const HoverUpdateContext = CC<(loc?: XY) => void>()
 
 export let hoverRef: DropInfo | undefined // access but won't trigger rerender on change
@@ -20,43 +28,43 @@ const DragContextProvider: FC<{ children?: React.ReactNode }> = ({
     children,
 }) => {
     const [drops, dropsDispatch] = useReducer(dropsReducer, [])
-    const [hover, setHover] = useState<DropInfo>()
 
-    const updateHover = (loc?: XY) => {
-        // console.log(loc)
+    const updateHover = useCallback(
+        (loc?: XY) => {
+            const newHover = drops.find(
+                (drop) =>
+                    loc &&
+                    loc.x >= drop.x &&
+                    loc.x <= drop.x + drop.width &&
+                    loc.y >= drop.y &&
+                    loc.y <= drop.y + drop.height
+            )
+            if (!dropInfoEqual(newHover, hoverRef)) {
+                hoverRef = newHover
+            }
+        },
+        [drops]
+    )
 
-        const newHover = drops.find(
-            (drop) =>
-                loc &&
-                loc.x >= drop.x &&
-                loc.x <= drop.x + drop.width &&
-                loc.y >= drop.y &&
-                loc.y <= drop.y + drop.height
-        )
-        if (!dropInfoEqual(newHover, hover)) {
-            // setHover(newHover);
-            hoverRef = newHover
-        }
-    }
-
-    const wasSuccessfulDrop = (glyph: string) => {
-        if (hoverRef?.glyph === glyph) return hover
-    }
+    const findDrop = useCallback(
+        (glyph?: string) => {
+            return drops.find((info) => info.glyph === glyph)
+        },
+        [drops]
+    )
 
     return (
-        <DropsContext.Provider value={drops}>
-            <DropsDispatchContext.Provider value={dropsDispatch}>
-                <HoverContext.Provider value={hoverRef}>
-                    <HoverUpdateContext.Provider value={updateHover}>
-                        <WasSuccessfulDropContext.Provider
-                            value={wasSuccessfulDrop}
-                        >
+        <DropsFindContext.Provider value={findDrop}>
+            <DropsContext.Provider value={drops}>
+                <DropsDispatchContext.Provider value={dropsDispatch}>
+                    <HoverContext.Provider value={hoverRef}>
+                        <HoverUpdateContext.Provider value={updateHover}>
                             {children}
-                        </WasSuccessfulDropContext.Provider>
-                    </HoverUpdateContext.Provider>
-                </HoverContext.Provider>
-            </DropsDispatchContext.Provider>
-        </DropsContext.Provider>
+                        </HoverUpdateContext.Provider>
+                    </HoverContext.Provider>
+                </DropsDispatchContext.Provider>
+            </DropsContext.Provider>
+        </DropsFindContext.Provider>
     )
 }
 
